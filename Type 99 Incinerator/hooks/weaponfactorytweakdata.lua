967905f04f02f702e89e0ca976bac712c516a4e9
Pawcio1337 <pawcio1337@gmail.com>
2022-06-30 23:04:55 +0200

    Final Weapon Fixes

--- Type 99 Incinerator/hooks/weaponfactorytweakdata.lua
+++ Type 99 Incinerator/hooks/weaponfactorytweakdata.lua
@@ -1,8 +1,8 @@
 Hooks:PostHook(WeaponFactoryTweakData, "create_bonuses", "Type99IncModInit", function(self)
 	self.parts.wpn_fps_fla_system_b_standard.unit = "units/mods/weapons/wpn_fps_fla_type99inc_pts/wpn_fps_fla_type99inc_barrel"
-	self.parts.wpn_fps_fla_system_b_standard.animations = {equip = "equip", unequip = "unequip", fire = "recoil", fire_steelsight = "recoil", reload = "reload", reload_not_empty = "reload"}
+	self.parts.wpn_fps_fla_system_b_standard.animations = {equip = "equip", unequip = "unequip", reload = "reload", reload_not_empty = "reload"}
 	self.parts.wpn_fps_fla_system_b_wtf.unit = "units/mods/weapons/wpn_fps_fla_type99inc_pts/wpn_fps_fla_type99inc_barrel_pilot_slim"
-	self.parts.wpn_fps_fla_system_b_wtf.animations = {equip = "equip", unequip = "unequip", fire = "recoil", fire_steelsight = "recoil", reload = "reload", reload_not_empty = "reload"}
+	self.parts.wpn_fps_fla_system_b_wtf.animations = {equip = "equip", unequip = "unequip", reload = "reload", reload_not_empty = "reload"}
 	self.parts.wpn_fps_fla_system_body_standard.unit = "units/mods/weapons/wpn_fps_fla_type99inc_pts/wpn_fps_fla_type99inc_rec"
 	self.parts.wpn_fps_fla_system_body_upper.unit = "units/payday2/weapons/wpn_upg_dummy/wpn_upg_dummy"
 	self.parts.wpn_fps_fla_system_dh_standard.unit = "units/mods/weapons/wpn_fps_fla_type99inc_pts/wpn_fps_fla_type99inc_handle"
